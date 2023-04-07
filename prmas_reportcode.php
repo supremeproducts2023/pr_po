f7b04cfd61c1fbe2dcb257ccfa97f3c6b8749796
narumol2022 <narumol.tho@hitera.com>
2023-04-07 14:59:49 +0700

    Fixed PHP Error => หน้าเพิ่ม PR ส่วนการออกรายงาน

--- prmas_reportcode.php
+++ prmas_reportcode.php
@@ -1,7 +1,6 @@
-<?
-@session_start();
-if(session_is_registered("empno_user")) {
-		require_once("../include_RedThemes/odbc_connect.php");				
+<?php
+if(isset($_SESSION["empno_user"])) {
+		require_once("../include_RedThemes/MSSQLServer_connect_2.php");				
 		$pr_no=@$_GET["pr_no"];
 
 			require_once("../include/open_report.php");				
@@ -13,10 +12,10 @@ if(session_is_registered("empno_user")) {
 				$del_pr_master = "delete from pr_master";
 				$del_pr_detail = "delete from pr_details";
 				
-				mysql_query($del_pr_master,$conn_mysql);
-				mysql_query($del_pr_detail,$conn_mysql);					
+				@odbc_exec($conn_mysql, $del_pr_master);
+				@odbc_exec($conn_mysql, $del_pr_detail);				
 				// ========= Insert Master ========= 		
-						$str_pr_master = "select format(p.pr_date,'YYYY-MM-DD') pr_date, '(' + p.deptno + ') ' + d.deptname deptname,
+						$str_pr_master = "select format(p.pr_date,'yyyy-MM-dd') pr_date, '(' + p.deptno + ') ' + d.deptname deptname,
 													pr_payment,flag_obj,obj_name1,obj_name2,obj_name3,estimate_day,
 													p.supplier_id,s.company_name,pr_remark,vat_include,
 													e1.e_name empname,e2.e_name mngname,mng_remark 
@@ -26,34 +25,35 @@ if(session_is_registered("empno_user")) {
 													left join emp e1 on p.empno=e1.empno
 													left join emp e2 on p.mngno=e2.empno
 													where p.pr_no='$pr_no'";
-						$cur_pr_master = odbc_exec($conn,$str_pr_master);	
+						$cur_pr_master = @odbc_exec($conn,$str_pr_master);	
 							
-						$pr_date=odbc_result($cur_pr_master, "pr_date");
-						$deptname=odbc_result($cur_pr_master, "deptname");
-						$empname=odbc_result($cur_pr_master, "empname");
-						$mngname=odbc_result($cur_pr_master, "mngname");
-						$pr_payment=odbc_result($cur_pr_master, "pr_payment");
-						$flag_obj=odbc_result($cur_pr_master, "flag_obj");
-						$obj_name1=odbc_result($cur_pr_master, "obj_name1");
-						$obj_name2=odbc_result($cur_pr_master, "obj_name2");
-						$obj_name3=odbc_result($cur_pr_master, "obj_name3");
-						$estimate_day=odbc_result($cur_pr_master, "estimate_day");				
-						if(odbc_result($cur_pr_master, "supplier_id")!=''){
-								$suppliername="(".odbc_result($cur_pr_master, "supplier_id").") ".odbc_result($cur_pr_master, "company_name");
+						$pr_date=@odbc_result($cur_pr_master, "pr_date");
+						$deptname=@odbc_result($cur_pr_master, "deptname");
+						$empname=@odbc_result($cur_pr_master, "empname");
+						$mngname=@odbc_result($cur_pr_master, "mngname");
+						$pr_payment=@odbc_result($cur_pr_master, "pr_payment");
+						$flag_obj=@odbc_result($cur_pr_master, "flag_obj");
+						$obj_name1=@odbc_result($cur_pr_master, "obj_name1");
+						$obj_name2=@odbc_result($cur_pr_master, "obj_name2");
+						$obj_name3=@odbc_result($cur_pr_master, "obj_name3");
+						$estimate_day=@odbc_result($cur_pr_master, "estimate_day");				
+						if(@odbc_result($cur_pr_master, "supplier_id")!=''){
+								$suppliername="(".@odbc_result($cur_pr_master, "supplier_id").") ".@odbc_result($cur_pr_master, "company_name");
 						}else{ $suppliername=''; }
-						$pr_remark=odbc_result($cur_pr_master, "pr_remark");
-						$vat_include=odbc_result($cur_pr_master, "vat_include");
+						$pr_remark=@odbc_result($cur_pr_master, "pr_remark");
+						$vat_include=@odbc_result($cur_pr_master, "vat_include");
 
 						if($flag_obj=="7"){
 							$obj_name1 = "Sub-Contact";
 						}
 						
-						if($vat_include=="0") $pr_vat = '�Ҥ��Թ��ҷ���к����Ҥ��Թ��ҷ���ѧ������ VAT';
-						else $pr_vat = '�Ҥ��Թ��ҷ���к����Ҥ��Թ��ҷ����� VAT ����';
+				
+						if($vat_include=="0") $pr_vat = 'ราคาสินค้าที่ระบุเป็นราคาสินค้าที่ยังไม่รวม VAT';
+						else $pr_vat = 'ราคาสินค้าที่ระบุเป็นราคาสินค้าที่รวม VAT แล้ว';
 						
 						$pr_remark =  $pr_vat."\n".$pr_remark;
 									
-						$mng_remark=odbc_result($cur_pr_master, "mng_remark");
+						$mng_remark=@odbc_result($cur_pr_master, "mng_remark");
 						
 						
 						$txt_addprmaster ="insert into pr_master (
@@ -70,23 +70,23 @@ if(session_is_registered("empno_user")) {
 															".chkINSMySQL($pr_remark).",".chkINSMySQL($mng_remark)."
 														)";
 						//echo  $txt_addprmaster.'<br>';
-						mysql_query($txt_addprmaster,$conn_mysql);
+						@odbc_exec($conn_mysql, $txt_addprmaster);			
 				// ========= Insert Detail ========= 		
 						$str_pr_details = "select  id,prod_no,prod_name,prod_qty,prod_unit,prod_price,ISNULL(discount_baht,0) discount_baht 
 														from pr_details 
 														where pr_no='$pr_no'
 														order by id";
-						$cur_pr_details = odbc_exec($conn,$str_pr_details);	
+						$cur_pr_details = @odbc_exec($conn,$str_pr_details);	
 
-						while(odbc_fetch_row($cur_pr_details)){ 
+						while(@odbc_fetch_row($cur_pr_details)){ 
 
-							$id=odbc_result($cur_pr_details, "id");	
-							$prod_no=odbc_result($cur_pr_details, "prod_no");	
-							$prod_name=odbc_result($cur_pr_details, "prod_name");	
-							$prod_qty=odbc_result($cur_pr_details, "prod_qty");	
-							$prod_unit=odbc_result($cur_pr_details, "prod_unit");	
-							$prod_price=odbc_result($cur_pr_details, "prod_price");	
-							$discount_baht=odbc_result($cur_pr_details, "discount_baht");	
+							$id=@odbc_result($cur_pr_details, "id");	
+							$prod_no=@odbc_result($cur_pr_details, "prod_no");	
+							$prod_name=@odbc_result($cur_pr_details, "prod_name");	
+							$prod_qty=@odbc_result($cur_pr_details, "prod_qty");	
+							$prod_unit=@odbc_result($cur_pr_details, "prod_unit");	
+							$prod_price=@odbc_result($cur_pr_details, "prod_price");	
+							$discount_baht=@odbc_result($cur_pr_details, "discount_baht");	
 
 							if((($prod_price*$prod_qty)-$discount_baht)==0)$price=0;
 							else $price = (($prod_price*$prod_qty)-$discount_baht) / $prod_qty;
@@ -97,11 +97,11 @@ if(session_is_registered("empno_user")) {
 															where sj.subjob=pd.subjob 
 															and pr_no= '$pr_no'
 															and id='$id'";
-											$curQUEDetailSubjob = odbc_exec($conn, $strQUEDetailSubjob );
+											$curQUEDetailSubjob = @odbc_exec($conn, $strQUEDetailSubjob );
 											$subjob_show="";
-											while(odbc_fetch_row($curQUEDetailSubjob)){			
-												if($subjob_show=="")$subjob_show =odbc_result($curQUEDetailSubjob,"subjob_show");	
-												else $subjob_show .= ",".odbc_result($curQUEDetailSubjob,"subjob_show");	
+											while(@odbc_fetch_row($curQUEDetailSubjob)){			
+												if($subjob_show=="")$subjob_show =@odbc_result($curQUEDetailSubjob,"subjob_show");	
+												else $subjob_show .= ",".@odbc_result($curQUEDetailSubjob,"subjob_show");	
 											}									
 											$prod_name .= "\n(".$subjob_show.")";
 										}
@@ -117,7 +117,7 @@ if(session_is_registered("empno_user")) {
 																		".chkINSMySQL($price)."
 																) ";
 							//echo  $txt_addprmaster.'<br>';
-							mysql_query($txt_addprdetails,$conn_mysql);
+							@odbc_exec($conn_mysql, $txt_addprdetails);	
 						}
 				// ========= Open Report ========= 												
 			if($flag_obj=="8")
